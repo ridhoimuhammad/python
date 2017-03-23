57721b5d879487871eac708cf8b16ce7e39165ad
ridhoi muhamad <ridho007@outlook.co.id>
2017-03-23 13:13:32 +0700

    belajar python

---
+++ variable.py
@@ -0,0 +1,18 @@
+#####################################
+# V A R I A B L E
+######################### /python/variable.py
+from __future__ import print_function
+def main():
+	#variable 
+	x = 800000
+	y = 4
+	z = x*y
+	st1 = 'jumlah biaya kost selama'
+	st2 ="bulan"
+
+	#tampil
+	print('biaya /bln',x)
+	print('lama',y)
+	print(st1,y,st2,z)
+if __name__ == "__main__":
+	main()
